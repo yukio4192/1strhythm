ce53b7699810118da7dfb4108e3415aedd787b2c
yukihiro <41922960a@gmail.com>
2019-05-28 16:52:05 +0900

    add start btn, write js push eventlister (blank)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
+var startBtn = document.getElementById("start-btn");
+var pushBtn = document.getElementById("push-btn");
+var resetBtn = document.getElementById("reset-btn");
+
+startBtn.addEventLister('click', gameStart());
+pushBtn.addEventLister('click', judge());
+resetBtn.addEventLister('click', gameInit());
+
+function gameInit(){
+
+}
+
+function gameStart(){
+
+}
+
+function timeCount(){
+
+}
+
 function judge(){
-  
+
 }
